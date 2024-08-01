e27e4433cf90c1043d15bef541921012fa6a19e6
JiayingYu152 <belleyu152@gmail.com>
2024-08-01 13:33:36 -0700

    refactor: Update FlightBooker component to reset form after submission

--- react-practice/src/practice/FlightBooker.jsx
+++ react-practice/src/practice/FlightBooker.jsx
@@ -4,16 +4,12 @@ import { useNavigate } from "react-router-dom";
 
 const formatDate = (date) => {
   //目的是将JavaScript的Date对象转换成一个格式化的日期字符串，具体格式为YYYY-MM-DD
-  console.log("before converting, date is: ", date); // Thu May 16 2024 18:06:05 GMT-0400 (Eastern Daylight Time)
 
   const year = date.getFullYear(); //getFullYear()直接返回一个数值，通常用于表示年份
   const month = (date.getMonth() + 1).toString().padStart(2, "0"); //getMonth()返回月份，范围从0（1月）到11（12月）。因此，在使用时需要加1来得到常规的月份表示（1-12月）。
   const day = date.getDate().toString().padStart(2, "0"); //返回月份中的日，范围从1到31
 
   // 对于月份和日期，使用了toString()方法将数值转换为字符串，这是因为你需要在这些值为个位数时用"0"进行填充，确保它们始终是两位数（例如，5月变为"05"）。这通过padStart(2, "0")实现，它确保字符串至少有两个字符的长度，如果不足则在前面填充"0"。
-
-  console.log("after converting, date becomes: ", [year, month, day].join("-")); // after converting, date becomes:  2024-05-16
-
   return [year, month, day].join("-"); //当数组中的元素通过join()方法连接成字符串时，所有的元素（包括数字）自动转换为字符串
 };
 
@@ -26,17 +22,21 @@ const FlightBooker = () => {
     formatDate(new Date(Date.now() + DAY_IN_SECONDS))
   );
   const [returnDate, setReturnDate] = useState(departureDate);
-  
+
   const navigate = useNavigate(); //for navigate back to home page, not main logic relevant
 
   const handleSumbit = (event) => {
     event.preventDefault(); //用来阻止事件的默认行为。对于表单提交事件，浏览器的默认行为是发送表单数据到服务器并重新加载页面。
     if (flightOption === "one-way") {
       alert(`You have booked a one-way flight on ${departureDate}`);
+      setDepartureDate(formatDate(new Date(Date.now() + DAY_IN_SECONDS))); //提交后，清空/重置表单
     } else {
       alert(
         `You have booked a return flight, departing on ${departureDate} and returning on ${returnDate}`
       );
+      //提交后，清空/重置表单
+      setDepartureDate(formatDate(new Date(Date.now() + DAY_IN_SECONDS)));
+      setReturnDate("");
     }
   };
 
@@ -48,6 +48,7 @@ const FlightBooker = () => {
       >
         Going back to home page
       </button>
+
       <form className="flight-booker-form-container" onSubmit={handleSumbit}>
         <select
           value={flightOption}
@@ -65,6 +66,7 @@ const FlightBooker = () => {
           value={departureDate}
           onChange={(event) => setDepartureDate(event.target.value)}
           min={TODAY}
+          required
         />
         {flightOption === "return" && (
           <input
@@ -73,6 +75,7 @@ const FlightBooker = () => {
             value={returnDate}
             onChange={(event) => setReturnDate(event.target.value)}
             min={departureDate}
+            required
           />
         )}
         <button type="submit">Book</button>
